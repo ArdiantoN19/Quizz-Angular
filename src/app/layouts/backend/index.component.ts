f5e6d869971676719cb9c477e3ea846e16a6992c
ardi <ardianto.nugroho@ifcagroup.com>
2025-01-23 14:14:22 +0700

    [feat]: add layout for dashboard admin

--- src/app/layouts/backend/index.component.ts
+++ src/app/layouts/backend/index.component.ts
@@ -1,9 +1,47 @@
-import { Component } from "@angular/core";
-import { RouterOutlet } from "@angular/router";
+import { Component, HostListener } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+import { MatButtonModule } from '@angular/material/button';
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatIconModule } from '@angular/material/icon';
+import { AuthService } from '../../services/authService/index.service';
+import { TAuthState } from '../../services/authService/index.type';
+import { MatMenuModule } from '@angular/material/menu';
 
 @Component({
-    selector: 'backend-layout-app',
-    templateUrl: 'index.component.html',
-    imports: [RouterOutlet]
+  selector: 'backend-layout-app',
+  templateUrl: 'index.component.html',
+  styleUrl: 'index.component.scss',
+  imports: [
+    RouterOutlet,
+    MatSidenavModule,
+    MatButtonModule,
+    MatIconModule,
+    MatMenuModule,
+  ],
 })
-export class BackendLayoutAppComponent {}
+export class BackendLayoutAppComponent {
+  private maxWidthTablet: number = 769;
+  isTabletMode: boolean = false;
+  authState: TAuthState | null = null;
+
+  constructor(private authService: AuthService) {}
+
+  ngOnInit(): void {
+    const innerWidth = window.innerWidth;
+    this.isTabletMode = innerWidth < this.maxWidthTablet;
+
+    const authState = this.authService.getAuthState();
+    this.authState = authState;
+  }
+
+  @HostListener('window:resize', [])
+  onResizeHandler() {
+    const innerWidth = window.innerWidth;
+    this.isTabletMode = innerWidth < this.maxWidthTablet;
+  }
+
+  onLogoutHandler(): void {
+    this.authService.logout()
+    this.authState = null;
+  }
+}
--- src/environments/environment.development.ts
+++ src/environments/environment.development.ts
@@ -1,7 +1,18 @@
 export const environment = {
     auth: {
         accountCredentialLocalStorageKey: 'ACCOUNT-REMEMBER',
-        secretKey: 'U2VtdWFBa2FuSW5kYWhQYWRhV2FrdHVueWEK'
+        authStateLocalStorageKey: 'AUTH-STATE',
+        secretKey: 'U2VtdWFBa2FuSW5kYWhQYWRhV2FrdHVueWEK',
+        expired: 1000 * 3600 * 24
     },
-    avatar: 'https://api.dicebear.com/9.x/adventurer/svg?seed'
+    avatar: 'https://api.dicebear.com/9.x/adventurer/svg?seed',
+    firebase: {
+        apiKey: "AIzaSyBklzYLZEC0OLnN21laAlLhGC0mWRBuLNg",
+        authDomain: "quizz-app-dbf4d.firebaseapp.com",
+        projectId: "quizz-app-dbf4d",
+        storageBucket: "quizz-app-dbf4d.firebasestorage.app",
+        messagingSenderId: "336205938373",
+        appId: "1:336205938373:web:c2e612047120848fe43aac",
+        measurementId: "G-7J4MVZV7EG"
+    }
 };
