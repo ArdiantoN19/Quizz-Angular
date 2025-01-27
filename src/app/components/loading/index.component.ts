5745c79b72c3d88a58de6281a050ee1c1e6f49a1
ardi <ardianto.nugroho@ifcagroup.com>
2025-01-27 15:15:18 +0700

    [feat]: add loading component

---
+++ src/app/components/loading/index.component.ts
@@ -0,0 +1,8 @@
+import { Component } from "@angular/core";
+
+@Component({
+    selector: 'loading-app',
+    templateUrl: 'index.component.html',
+    styleUrl: 'index.component.scss',
+})
+export class LoadingAppComponent {}
