ea09e20e0330194da624f917001e732a852e3dcd
ardi <ardianto.nugroho@ifcagroup.com>
2025-02-25 14:48:45 +0700

    [feat]: add alert component

---
+++ src/app/components/alert/index.component.ts
@@ -0,0 +1,35 @@
+import { Component, ElementRef, Input, ViewChild } from '@angular/core';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
+import { MatTooltipModule } from '@angular/material/tooltip';
+
+type TAlert = 'danger' | 'warning' | 'success' | 'info';
+
+const iconType: Record<TAlert, string> = {
+  danger: 'cancel',
+  info: 'warning',
+  warning: 'warning',
+  success: 'check_circle',
+};
+
+@Component({
+  selector: 'alert-app',
+  templateUrl: 'index.component.html',
+  styleUrl: 'index.component.scss',
+  imports: [MatIconModule, MatButtonModule, MatTooltipModule],
+})
+export class AlertAppComponent {
+  @Input({ required: true }) type!: TAlert;
+  @Input({ required: true }) message!: string;
+  @Input({ required: true }) title!: string;
+
+  @ViewChild('alert') alert!: ElementRef;
+
+  get iconType(): string {
+    return iconType[this.type];
+  }
+
+  onDismissAlertHandler() {
+    this.alert.nativeElement.style.display = 'none'
+  }
+}
