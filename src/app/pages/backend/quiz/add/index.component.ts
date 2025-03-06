55d398b16b974edd3d99abfa8cc9e33b412eced2
ardi <ardianto.nugroho@ifcagroup.com>
2025-03-06 10:05:21 +0700

    [feat]: add back button from add quiz page to quiz list page

--- src/app/pages/backend/quiz/add/index.component.ts
+++ src/app/pages/backend/quiz/add/index.component.ts
@@ -1,16 +1,26 @@
-import { Component } from "@angular/core";
+import { Component, inject } from "@angular/core";
 import { TitleBackendAppComponent } from "../../../../components/backend/title/index.component";
 import { QuizStepperAppComponent } from "../../../../components/backend/quiz/stepper/index.component";
+import { MatButtonModule } from "@angular/material/button";
+import { MatIconModule } from "@angular/material/icon";
+import { MatTooltipModule } from "@angular/material/tooltip";
+import { Router } from "@angular/router";
 
 @Component({
     selector: 'quiz-add-page-app',
     templateUrl: 'index.component.html',
     styleUrl: 'index.component.scss',
-    imports: [TitleBackendAppComponent, QuizStepperAppComponent]
+    imports: [TitleBackendAppComponent, QuizStepperAppComponent, MatButtonModule, MatIconModule, MatTooltipModule]
 })
 export class QuizAddPageApp {
     dataTitle = {
         title: 'Add Quiz',
         subtitle: 'You can add and setup your quiz here.'
+    }
+
+    private router = inject(Router)
+
+    onBackHandler() {
+        this.router.navigate(['/admin/quiz',])
     }
 }
